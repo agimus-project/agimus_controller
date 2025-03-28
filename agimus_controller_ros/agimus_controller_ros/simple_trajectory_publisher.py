7d592c03701b926c8f1d5fc785222a77b715544e
Maximilien Naveau <maximilien.naveau@gmail.com>
2025-03-28 17:45:57 +0100

    Update the simple_trajectory_publisher with a parameter for ee_frame_name and update robot model loading

--- agimus_controller_ros/agimus_controller_ros/simple_trajectory_publisher.py
+++ agimus_controller_ros/agimus_controller_ros/simple_trajectory_publisher.py
@@ -9,11 +9,16 @@ from rclpy.node import Node
 import rclpy
 from rclpy.qos import QoSProfile, DurabilityPolicy, ReliabilityPolicy
 from sensor_msgs.msg import JointState
+from rcl_interfaces.srv import GetParameters
+from rcl_interfaces.msg import ParameterValue
 
+from agimus_controller.factory.robot_model import (
+    RobotModelParameters,
+    RobotModels,
+)
 from agimus_controller_ros.trajectory_weights_parameters import (
     trajectory_weights_params,
 )
-
 
 class QuinticTrajectory:
     """Computes a quintic polynomial trajectory with desired amplitude and duration."""
@@ -54,9 +59,9 @@ class SimpleTrajectoryPublisher(Node):
         self.pin_model = None
         self.pin_data = None
         self.ee_frame_id = None
-        self.ee_frame_name = "fer_link8"
         self.param_listener = trajectory_weights_params.ParamListener(self)
         self.params = self.param_listener.get_params()
+        self.ee_frame_name = self.params.ee_frame_name
         self.robot_description_msg = None
 
         self.q0 = None
@@ -99,10 +104,30 @@ class SimpleTrajectoryPublisher(Node):
                 reliability=ReliabilityPolicy.BEST_EFFORT,
             ),
         )
+        self.moving_joint_names = self.get_param_from_node(
+            "linear_feedback_controller", "moving_joint_names"
+        ).string_array_value
         self.timer = self.create_timer(
             0.01, self.publish_mpc_input
         )  # Publish at 100 Hz
         self.get_logger().info("Simple trajectory publisher node started.")
+
+    def get_param_from_node(self, node_name: str, param_name: str) -> ParameterValue:
+        """Returns parameter from the node"""
+        param_client = self.create_client(GetParameters, f"/{node_name}/get_parameters")
+        while not param_client.wait_for_service(timeout_sec=1.0):
+            self.get_logger().info("Service not available, waiting again...")
+        request = GetParameters.Request()
+        request.names = [param_name]
+
+        future = param_client.call_async(request)
+        rclpy.spin_until_future_complete(self, future)
+
+        if future.result() is not None:
+            return future.result().values[0]
+        else:
+            raise ValueError("Failed to load moving joint names from LFC")
+
 
     def joint_states_callback(self, joint_states_msg: JointState) -> None:
         """Set joint state reference."""
@@ -122,14 +147,36 @@ class SimpleTrajectoryPublisher(Node):
 
     def load_models(self):
         """Callback to get robot description and store to object"""
-        self.pin_model = pin.buildModelFromXML(self.robot_description_msg.data)
+        self.robot_models = RobotModels(param=RobotModelParameters(
+            robot_urdf = self.robot_description_msg.data,
+            free_flyer = False,
+            moving_joint_names = self.moving_joint_names
+        ))
+        self.pin_model = self.robot_models.robot_model
         self.pin_data = self.pin_model.createData()
         self.ee_frame_id = self.pin_model.getFrameId(self.ee_frame_name)
 
+        self.get_logger().warn(f"Model loaded, self.q0 = {self.q0.size}")
+        self.get_logger().warn(f"Model loaded, nb_joints = {len(self.robot_models.full_robot_model.joints)}")
+        # Note: remove 7 to joint id if free flyer is used.
+        # joint id starts with 1 because joint id 0 is the universe.
+        q0 = np.array([
+            self.q0[
+                self.robot_models.full_robot_model.idx_qs[
+                    self.robot_models.full_robot_model.getJointId(jn)
+                ]
+            ]
+            for jn in self.robot_models.params.moving_joint_names
+        ])
+        self.q0 = q0.copy()
         self.q = self.q0.copy()
         self.dq = np.zeros_like(self.q)
         self.ddq = np.zeros_like(self.q)
-        self.get_logger().warn(f"Model loaded, pin_model.nq = {self.pin_model.nq}")
+
+        self.get_logger().warn(f"Model loaded, reduced self.q0 = {self.q0}")
+        self.get_logger().warn(
+            f"Model loaded, reduced self.q0 size = {self.q0.size}")
+        self.get_logger().warn(f"Model loaded, pin_model = {self.pin_model}")
 
     def get_weights(
         self, weights: List[np.float64], size: np.float64
