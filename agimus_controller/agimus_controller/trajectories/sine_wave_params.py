055b4be94d4977d8cb8a0201a813af66e0246fc5
Maximilien Naveau <maximilien.naveau@gmail.com>
2025-04-24 16:39:29 +0200

    Add the sine wave param class

---
+++ agimus_controller/agimus_controller/trajectories/sine_wave_params.py
@@ -0,0 +1,37 @@
+import numpy as np
+
+
+class SinWaveParams:
+    """
+    Class to store parameters for a sine wave trajectory.
+    """
+
+    def __init__(self, amplitude: float, period: float, scale_duration: float):
+        """
+        Initialize the sine wave parameters.
+
+        :param amplitude: Amplitude of the sine wave.
+        :param period: Period of the sine wave.
+        :scale_duration: Duration from 0 velocity to the sin wave velocity.
+        """
+        self.amplitude = amplitude
+        self.period = period
+        self.scale_duration = scale_duration
+
+    @property
+    def pulsation(self) -> float:
+        """
+        Calculate the pulsation of the sine wave.
+
+        :return: Pulsation of the sine wave.
+        """
+        return 2 * np.pi / self.period
+
+    @property
+    def frequency(self) -> float:
+        """
+        Calculate the frequency of the sine wave.
+
+        :return: Frequency of the sine wave.
+        """
+        return 1.0 / self.period
