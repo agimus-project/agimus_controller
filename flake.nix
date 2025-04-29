0b8e26d345bf163bfccbf2a8575a1130682f12c2
Guilhem Saurel <guilhem.saurel@laas.fr>
2025-04-29 11:52:34 +0200

    CI: aarch64-darwin

--- flake.nix
+++ flake.nix
@@ -22,7 +22,10 @@
       );
     in
     inputs.flake-parts.lib.mkFlake { inherit inputs; } {
-      systems = [ "x86_64-linux" ];
+      systems = [
+        "x86_64-linux"
+        "aarch64-darwin"
+      ];
       imports = [ inputs.treefmt-nix.flakeModule ];
       perSystem =
         {
