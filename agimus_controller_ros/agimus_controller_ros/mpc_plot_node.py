43aed6a57d5d46add141fa2c3dcb1fffa0165c4f
Maximilien Naveau <maximilien.naveau@gmail.com>
2025-07-09 14:40:23 +0200

    Add the mpc plot node

---
+++ agimus_controller_ros/agimus_controller_ros/mpc_plot_node.py
@@ -0,0 +1,80 @@
+from ament_index_python import get_package_share_directory
+from pathlib import Path
+import xacro
+
+from agimus_controller.plots.plots_utils import plot_mpc_data
+from agimus_controller.factory.robot_model import RobotModelParameters, RobotModels
+from agimus_controller_ros.read_from_bag_trajectory import (
+    load_mpc_ouputs_from_rosbag,
+)
+
+
+def plot_mpc(args) -> None:
+    bag_file_path = Path(args.bag_file_path)
+    if "panda" in bag_file_path.stem:
+        config_folder_path = (
+            Path(get_package_share_directory("agimus_demo_03_mpc_dummy_traj"))
+            / "config"
+        )
+        env_xacro_path = (
+            Path(get_package_share_directory("agimus_demo_05_pick_and_place"))
+            / "urdf"
+            / "environment.urdf.xacro"
+        )
+        robot_models = RobotModels(
+            RobotModelParameters.from_panda(config_folder_path, env_xacro_path)
+        )
+    elif (
+        "tiago_pro" in bag_file_path.stem.lower()
+        or "tiago-pro" in bag_file_path.stem.lower()
+    ):
+        tiago_pro_pkg = Path(get_package_share_directory("tiago_pro_description"))
+        tiago_pro_urdf_path = tiago_pro_pkg / "robots" / "tiago_pro.urdf.xacro"
+        tiago_pro_urdf = xacro.process_file(tiago_pro_urdf_path).toxml()
+        moving_joint_names = [
+            "arm_right_1_joint",
+            "arm_right_2_joint",
+            "arm_right_3_joint",
+            "arm_right_4_joint",
+            "arm_right_5_joint",
+            "arm_right_6_joint",
+            "arm_right_7_joint",
+        ]
+        robot_models = RobotModels(
+            RobotModelParameters.from_tiago_pro(tiago_pro_urdf, moving_joint_names)
+        )
+    else:
+        raise ValueError(
+            "Unsupported robot type in bag file name. "
+            "Please use a bag file with 'panda' or 'tiago_pro' in its name."
+        )
+
+    mpc_data = load_mpc_ouputs_from_rosbag(args.bag_file_path)
+    which_plots = [
+        "computation_time",
+        "collision_distance",
+        "iter",
+        "visual_servoing",
+        "predictions",
+    ]
+    plot_mpc_data(mpc_data, robot_models.robot_model, which_plots)
+    return
+
+
+def main():
+    import argparse
+
+    parser = argparse.ArgumentParser(description="Plot MPC data from a ROS bag file.")
+    parser.add_argument(
+        "--bag-file-path",
+        "-p",
+        type=str,
+        required=True,
+        help="Path to the ROS bag file containing MPC data.",
+    )
+    args = parser.parse_args()
+    plot_mpc(args)
+
+
+if __name__ == "__main__":
+    main()
