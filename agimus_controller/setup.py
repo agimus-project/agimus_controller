f0b4271928b1b0bb6d54bae42a95695625c38ba0
Maximilien Naveau <maximilien.naveau@gmail.com>
2026-04-23 21:44:26 +0200

    better package agimus_controller

--- agimus_controller/setup.py
+++ agimus_controller/setup.py
@@ -1,17 +1,28 @@
+from pathlib import Path
 from setuptools import find_packages, setup
+from typing import List
+
 
 PACKAGE_NAME = "agimus_controller"
 REQUIRES_PYTHON = ">=3.10.0"
+
+
+def get_files(dir: Path, pattern: str) -> List[str]:
+    return [x.as_posix() for x in (dir).glob(pattern) if x.is_file()]
+
 
 setup(
     name=PACKAGE_NAME,
     version="0.0.0",
     packages=find_packages(exclude=["tests"]),
-    package_dir={"./agimus_controller": "agimus_controller"},
-    package_data={"agimus_controller": ["ocp/*.yaml", "package.xml"]},
+    package_dir={f"./{PACKAGE_NAME}": PACKAGE_NAME},
+    package_data={"agimus_controller": ["ocp/*.yaml"]},
     data_files=[
         ("share/ament_index/resource_index/packages", ["resource/" + PACKAGE_NAME]),
-        ("share/" + PACKAGE_NAME, ["ocp/*.yaml", "package.xml"]),
+        (
+            "share/" + PACKAGE_NAME,
+            ["package.xml"],
+        ),
     ],
     python_requires=REQUIRES_PYTHON,
     install_requires=[
